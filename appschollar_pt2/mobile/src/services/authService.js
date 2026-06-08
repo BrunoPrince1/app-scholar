6667774ec38dfb58bd3976c8fb217e599a837d75
BrunoPrince1 <brunoprince.m@gmail.com>
2026-06-08 19:25:27 -0300

    Delete appschollar_pt2 directory

--- appschollar_pt2/mobile/src/services/authService.js
+++
@@ -1,6 +0,0 @@
-import api from './api';
-
-export async function loginApi(email, senha) {
-  const response = await api.post('/api/login', { email, senha });
-  return response.data; // { token, usuario }
-}
