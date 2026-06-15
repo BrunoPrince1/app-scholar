40e982a0760b9c3f20e5846a4a6f0c81ea3390d1
BrunoPrince1 <brunoprince.m@gmail.com>
2026-06-15 19:45:01 -0300

    Delete mobile (front) antigo

--- mobile/src/services/mockData.js
+++
@@ -1,5 +0,0 @@
-export const mockBoletim = [
-  { id: 1, disciplina: 'Programação para Dispositivos Móveis', nota1: 8.5, nota2: 9.0, media: 8.75, situacao: 'Aprovado' },
-  { id: 2, disciplina: 'Desenvolvimento Web', nota1: 7.0, nota2: 6.5, media: 6.75, situacao: 'Aprovado' },
-  { id: 3, disciplina: 'Banco de Dados', nota1: 5.5, nota2: 4.0, media: 4.75, situacao: 'Reprovado' },
-];
